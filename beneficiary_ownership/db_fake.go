e5d44be8c71c6167df3dabf9a7262750c33e445b
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-06-03 23:01:53 +0700

    feat: add cors

---
+++ beneficiary_ownership/db_fake.go
@@ -0,0 +1,8 @@
+package beneficiary_ownership
+
+import models "lexicon/bo-api/beneficiary_ownership/v1/models"
+
+// 'in memory' fake database, so to speak
+
+var search_results []models.SearchResultModel
+var detail_results []models.DetailResultModel
--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	bo "lexicon/bo-api/beneficiary_ownership"
 	bo_v1 "lexicon/bo-api/beneficiary_ownership/v1"
 	"net/http"
+	"time"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
@@ -37,6 +39,18 @@ func main() {
 	bo.SetDatabase(pgsqlClient)
 
 	r := chi.NewRouter()
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://bo.lexicon.id", "http://localhost:3000"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-API-KEY", "X-ACCESS-TIME", "X-REQUEST-SIGNATURE", "X-API-USER", "X-REQUEST-IDENTITY"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
@@ -45,7 +59,7 @@ func main() {
 	// // Set a timeout value on the request context (ctx), that will signal
 	// // through ctx.Done() that the request has timed out and further
 	// // processing should be stopped.
-	// r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.Timeout(2 * time.Minute))
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Mount("/beneficiary-ownership", bo_v1.Router())
